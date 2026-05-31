26f72994f7151677c877873ac4afc54cbafabc7e
xrickastley <reodiqueridge10@gmail.com>
2026-05-31 23:51:00 +0800

    Gamerule for Overloaded creating Fire (#12)

--- src/main/java/io/github/xrickastley/sevenelements/element/reaction/OverloadedElementalReaction.java
+++ src/main/java/io/github/xrickastley/sevenelements/element/reaction/OverloadedElementalReaction.java
@@ -52,7 +52,7 @@ public class OverloadedElementalReaction extends ElementalReaction {
 			y,
 			z,
 			power,
-			true,
+			world.getGameRules().getBoolean(SevenElementsGameRules.OVERLOADED_EXPLOSIONS_CREATE_FIRE),
 			world.getGameRules().getBoolean(SevenElementsGameRules.OVERLOADED_EXPLOSIONS_DAMAGE_BLOCKS)
 				? DestructionType.DESTROY
 				: DestructionType.KEEP
