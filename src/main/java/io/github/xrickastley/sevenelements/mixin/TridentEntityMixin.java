4659e81355c215b0faec581883f0c264721d89e4
xrickastley <reodiqueridge10@gmail.com>
2026-07-08 18:33:28 +0800

    Fixed thrown Tridents not applying their elemental infusions on hit.

---
+++ src/main/java/io/github/xrickastley/sevenelements/mixin/TridentEntityMixin.java
@@ -0,0 +1,32 @@
+package io.github.xrickastley.sevenelements.mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.projectile.TridentEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TridentEntity.class)
+public abstract class TridentEntityMixin extends PersistentProjectileEntity {
+	protected TridentEntityMixin(EntityType<? extends PersistentProjectileEntity> entityType, World world) {
+		super(entityType, world);
+
+		throw new AssertionError();
+	}
+
+	@Inject(
+		method = "<init>(Lnet/minecraft/world/World;Lnet/minecraft/entity/LivingEntity;Lnet/minecraft/item/ItemStack;)V",
+		at = @At(
+			value = "INVOKE_ASSIGN",
+			target = "Lnet/minecraft/item/ItemStack;copy()Lnet/minecraft/item/ItemStack;"
+		)
+	)
+	private void addTridentInfusions(World world, LivingEntity owner, ItemStack stack, CallbackInfo ci) {
+		this.sevenelements$setOriginStack(stack);
+	}
+}
