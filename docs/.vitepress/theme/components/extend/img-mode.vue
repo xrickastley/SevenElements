16d3eac59d0344eae95b2517b4979b9e3fab3f22
xrickastley <reodiqueridge10@gmail.com>
2026-07-15 23:09:38 +0800

    Fixed img-mode implementation for first view.

--- docs/.vitepress/theme/components/extend/img-mode.vue
+++ docs/.vitepress/theme/components/extend/img-mode.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useData, withBase } from 'vitepress';
-import { useAttrs } from 'vue';
+import { computed, useAttrs } from 'vue';
 
 const { isDark } = useData();
 const attrs = useAttrs();
@@ -9,11 +9,9 @@ const props = defineProps<{
 	lightSrc: string;
 }>();
 
-const darkSrc = withBase(props.darkSrc);
-const lightSrc = withBase(props.lightSrc);
+const src = computed(() => withBase(isDark.value || import.meta.env.SSR ? props.darkSrc : props.lightSrc));
 </script>
 
 <template>
-	<img v-bind="attrs" v-if="isDark" :src="darkSrc">
-	<img v-bind="attrs" v-else :src="lightSrc">
+	<img v-bind="attrs" :src="src">
 </template>
